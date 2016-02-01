2415f796808756da5f817ba2a04c4cb90934ec5f
karang24 <tunggal.jaka@gmail.com>
2016-02-02 06:07:03 +0700

    team4

--- diagram-view.php
+++ diagram-view.php
@@ -68,7 +68,32 @@ case "t";
 	$tahun=$_GET['tahun'];
 	
 ?>
-
+$tampil=mysql_db_query($db,"select * from tblpenyebaran where tahun='$tahun' order by nama_penyakit asc",$koneksi);
+	$tampil2=mysql_db_query($db,"select * from tblpenyebaran where tahun='$tahun' order by nama_penyakit desc ",$koneksi);
+	
+	$jumlah=mysql_num_rows($tampil);
+	if ($jumlah){
+		?><font face="verdana" size="2" color="#0033FF">
+		<b>Query : </b> Menampilkan data semua penyakit pada tahun <i><font color="#009900"><b><? echo $tahun; ?>.</b></font></i>
+		</font>
+		<p align="center">
+		<table width="340" border="0" align="center" cellpadding="0" cellspacing="0">
+		<?
+		
+		//mencari total jumlah penderita
+		while($row2=mysql_fetch_row($tampil2))
+		{
+			$nilai=$row2[3];
+			$tambah=$nilai+$tambah;
+		}
+			$total=$tambah;
+		
+		
+		while($row=mysql_fetch_row($tampil))
+		{
+		
+			$persen=round(((int)$row[3]/(int)$total)*100,2);
+			$lebar=$persen*2;
 
 
 
